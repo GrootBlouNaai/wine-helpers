e4a0b5aaa7b0ba68a74dcc4cfde6abbe503448c6
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-04 22:13:34 +0200

    Update helpers.php

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -27,22 +27,12 @@ if (!function_exists('app')) {
 
 if (!function_exists('debug_string_backtrace')) {
     function debug_string_backtrace($text = null, $config = null) {
-
-        /** @var Config $config */
         $config = null === $config ? app('start')->getConfig() : $config;
-
-        if (is_string($config)) {
-            $file = $config;
-        } else {
-            $file = $config->getLogsDir() . '/debug.log';
-        }
-
+        $file = is_string($config) ? $config : $config->getLogsDir() . '/debug.log';
 
         static $init;
-
         if (null === $init) {
             $init = true;
-
             if (file_exists($file)) {
                 @unlink($file);
             }
@@ -50,9 +40,7 @@ if (!function_exists('debug_string_backtrace')) {
 
         if (null === $text) {
             ob_start();
-
             static $time;
-
             if (null === $time) {
                 $time = microtime(true);
                 print "Time: 0\n\n";
@@ -64,28 +52,21 @@ if (!function_exists('debug_string_backtrace')) {
             $trace = ob_get_contents();
             ob_end_clean();
 
-            // Remove first item from backtrace as it's this function which
-            // is redundant.
-            $trace = preg_replace ('/^#0\s+' . __FUNCTION__ . "[^\n]*\n/", '', $trace, 1);
-
-            // Renumber backtrace items.
-            $trace = preg_replace ('/^#(\d+)/me', '\'#\' . ($1 - 1)', $trace);
-
-            $trace = implode("\n", array_map(function ($n) {list($a) = explode('called at', $n); return trim($a);}, explode("\n", $trace)));
+            $trace = preg_replace('/^#0\s+' . __FUNCTION__ . "[^\n]*\n/", '', $trace, 1);
+            $trace = preg_replace('/^#(\d+)/me', '\'#\' . ($1 - 1)', $trace);
+            $trace = implode("\n", array_map(function ($n) {
+                list($a) = explode('called at', $n);
+                return trim($a);
+            }, explode("\n", $trace)));
         } else {
             $trace = $text;
         }
 
         file_put_contents($file, "{$trace}\n\n", FILE_APPEND);
-
         return "{$trace}\n\n";
     }
 }
 
-/* *
- * startsWith('http://example.com', 'http://') -> true
- * startsWith('http://example.com', 'ttp://')  -> false
- * */
 if (!function_exists('startsWith')) {
     /**
      * @param string       $haystack
@@ -93,8 +74,7 @@ if (!function_exists('startsWith')) {
      *
      * @return bool
      */
-    function startsWith($haystack, $needle)
-    {
+    function startsWith($haystack, $needle) {
         if (is_array($needle)) {
             foreach ($needle as $str) {
                 if (strpos($haystack, $str) === 0) {
@@ -103,15 +83,10 @@ if (!function_exists('startsWith')) {
             }
             return false;
         }
-
         return (string)$needle === "" || strpos($haystack, (string)$needle) === 0;
     }
 }
 
-/**
- * endWith('http://example.com', 'om')         -> true
- * endWith('http://example.com', '.co')        -> false
- */
 if (!function_exists('endsWith')) {
     /**
      * @param string       $haystack
@@ -119,8 +94,7 @@ if (!function_exists('endsWith')) {
      *
      * @return bool
      */
-    function endsWith($haystack, $needle)
-    {
+    function endsWith($haystack, $needle) {
         if (is_array($needle)) {
             foreach ($needle as $str) {
                 if (substr($haystack, -strlen($str)) === $str) {
@@ -129,7 +103,6 @@ if (!function_exists('endsWith')) {
             }
             return false;
         }
-
-        return (string)$needle === "" || substr($haystack, (string)-strlen($needle)) === $needle;
+        return (string)$needle === "" || substr($haystack, -strlen($needle)) === $needle;
     }
 }
